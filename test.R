79046bf62f291a4d5b50517f507b98efd17af812
LHHJ <30591189+lhhj@users.noreply.github.com>
2021-06-15 12:00:37 +0000

    changes to docker file and test

--- test.R
+++ test.R
@@ -1,3 +1,11 @@
-v<-1:20
-vx<-1:20
-plot(v,vx)
+library(AzureStor)
+
+keystring<-"EnalwGNW0OHCFLrogamszCu129rn5W687VVOebGcGqvyQXic7mongIHPdDoQkNEMVavicRK1vVy82hOOjBLAXg=="
+account_name <- "https://lakefsscidemo.blob.core.windows.net/"
+
+bl_endp_key<- storage_endpoint(account_name,key=keystring)
+cont<-storage_container(bl_endp_key,"datablob")
+dataInput <- storage_read_csv(cont,"world-happiness-report-2021.csv")
+storage_write_csv(dataInput,cont,as.character(Sys.time(), usetz = TRUE))
+Sys.time()
+
