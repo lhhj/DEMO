4a2c4effb4dad0d93953a897fae9aba8ffffe2e6
LHHJ <30591189+lhhj@users.noreply.github.com>
2021-06-15 12:28:21 +0000

    d

--- test.R
+++ test.R
@@ -7,5 +7,4 @@ bl_endp_key<- storage_endpoint(account_name,key=keystring)
 cont<-storage_container(bl_endp_key,"datablob")
 dataInput <- storage_read_csv(cont,"world-happiness-report-2021.csv")
 storage_write_csv(dataInput,cont,as.character(Sys.time(), usetz = TRUE))
-Sys.time()
 
