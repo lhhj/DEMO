67cec786bd0c9bce0b2808c5c08200578d58110d
LHHJ <30591189+lhhj@users.noreply.github.com>
2021-06-17 09:45:22 +0000

    lots of log and change of automated run

--- Runner.R
+++ Runner.R
@@ -1,11 +1,14 @@
+print("before")
 library(AzureStor,lib.loc='/CustomR')
-
+print("after load")
 keystring<-"ts5KGX0irB9tTPCOzo89GC6KJTc0UM9bpUvJLLnP2nyUxZ5sEVokQb1iO3pGdNP6M7l33jQslBaE5cjIBZQUOQ=="
 account_name <- "https://scidemostorage.blob.core.windows.net/"
-
-
+print("before enc")
 bl_endp_key<- storage_endpoint(account_name,key=keystring)
 cont<-storage_container(bl_endp_key,"datablob")
 dataInput <- storage_read_csv(cont,"world-happiness-report-2021.csv")
+print("after get")
+print(dataInput)
 storage_write_csv(dataInput,cont,as.character(Sys.time(), usetz = TRUE))
+print("after write")
 
