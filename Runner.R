8e524949abae1ed513c96395aa4cba9a65e68661
LHHJ <30591189+lhhj@users.noreply.github.com>
2021-06-21 21:49:51 +0000

    added env

--- Runner.R
+++ Runner.R
@@ -1,8 +1,11 @@
+#Loading AzureStore file
 print("before")
 library(AzureStor,lib.loc='/CustomR')
+#azurestore loaded
 print("after load")
 keystring<-"ts5KGX0irB9tTPCOzo89GC6KJTc0UM9bpUvJLLnP2nyUxZ5sEVokQb1iO3pGdNP6M7l33jQslBaE5cjIBZQUOQ=="
 account_name <- "https://scidemostorage.blob.core.windows.net/"
+#before access attempt
 print("before enc")
 bl_endp_key<- storage_endpoint(account_name,key=keystring)
 cont<-storage_container(bl_endp_key,"datablob")
